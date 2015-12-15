0b41f8eeb9b06b7de27e0dc428bb8013bb4141bd
Dimitris Paxinos <dpaxinos@gmail.com>
2015-12-15 11:30:13 +0100

    Woeking solution before moving to a more complex type

---
+++ WpfAutoCompleteTextBoxWithAkka/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAutoCompleteTextBoxWithAkka
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public static IEnumerable<Contact> GetContacts()
+        {
+            var contacts = new List<Contact>();
+            contacts.Add(new Contact() { FirstName = "Dimitris", LastName = "Paxinos" });
+            contacts.Add(new Contact() { FirstName = "Sebastian", LastName = "Braune" });
+            contacts.Add(new Contact() { FirstName = "Ingo", LastName = "Strauch" });
+            contacts.Add(new Contact() { FirstName = "Manuel", LastName = "Saegebrecht" });
+            contacts.Add(new Contact() { FirstName = "Vladimir", LastName = "Tasic" });
+        }
+
+    }
+}
---
+++ WpfAutoCompleteTextBoxWithAkka/ViewModels/AutoCompleteTextBoxContactViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WpfAutoCompleteTextBoxWithAkka.ViewModels
+{
+    public class AutoCompleteTextBoxContactViewModel : AutoCompleteTextBoxViewModelBase<Contact>
+    {
+        public AutoCompleteTextBoxContactViewModel(Contact itemToBeSet)
+            : base(itemToBeSet)
+        {
+        }
+
+        public async override Task<IEnumerable<Contact>> GetItems(string text)
+        {
+            return Contact.GetContacts().Where(x => x.LastName.StartsWith(text, true, CultureInfo.InvariantCulture));
+        }
+    }
+}
